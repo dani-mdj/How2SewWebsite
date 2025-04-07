6701dfefd33586b5c5618569451192711cad5470
Danielle <166341005+dani-mdj@users.noreply.github.com>
2025-04-07 15:33:01 -0400

    Added Project Gallery
    Added images in project gallery (some images not included)

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@ const dteNow = new Date();
 const intYear = dteNow.getFullYear();
 
 copyright.innerHTML = "&copy " + intYear + " Copyright Danielle Jenson";
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("img-slides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
